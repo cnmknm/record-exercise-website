0c4fcebe6ba2d3b687f949714490ead7d246c9d6
cnmknm <65263546+cnmknm@users.noreply.github.com>
2021-08-15 15:55:20 -0700

    Add files via upload

---
+++ exercises_controller.mjs
@@ -0,0 +1,79 @@
+import * as exercises from './exercises_model.mjs';
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+
+app.post('/exercises', (req, res) => {
+    exercises.createExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(exercise => {
+            res.status(201).json(exercise);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ Error: 'Request failed' });
+        });
+});
+
+
+app.get('/exercises', (req, res) => {
+    let filter;
+    exercises.findExercises(filter, "", 0)
+        .then(exercises => {
+            if (exercises !== null) {
+                res.status(200).json(exercises);
+            } else {
+                res.status(500).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ Error: 'Request failed' });
+        });
+});
+
+
+app.put('/exercises/:_id', (req, res) => {
+    exercises.replaceExercise(req.params._id, req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(numUpdated => {
+            if (numUpdated === 1) {
+                res.status(200).json({ _id: req.params._id, name: req.body.name, reps: req.body.reps, weight: req.body.weight, unit: req.body.unit, date: req.body.date })
+            } else {
+                res.status(500).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ Error: 'Request failed' });
+        });
+});
+
+
+app.delete('/exercises/:_id', (req, res) => {
+    exercises.deleteById(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(500).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.send({ error: 'Request failed' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
